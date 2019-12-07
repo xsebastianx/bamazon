8db9f1500ff387ff380de7343377ba2fa1138e9d
Sebastian Arnavat <sebastianarnavat@Sebastians-MacBook-Pro.local>
2019-12-07 11:20:47 -0500

    Code for Bamazon Customers

---
+++ bamazonCustomer.js
@@ -0,0 +1,73 @@
+const mysql = require("msql");
+const inquirer = require("inquier");
+const table = require("cli-table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
+
+connection.connect(function(err){
+    if(err)throw err;
+    console.log("Connected as id" + connection.threadId);    
+});
+
+const displayProducts = function(){
+    const query = "SELECT * FROM products";
+    connection.query(query, function(err, res){
+        if(err) throw err;
+        const displayTable = new Table ({
+            head: ["Item ID", "Product", "Department", "Price", "Quantity"],
+            colWidths: [10,25,25,10,14]
+        });
+        for(var i = 0; i < res.length; i++){
+            displayTable.push(
+                [res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity] 
+            );
+        }
+        console.log(displayTable.toString());
+        purchasePrompt();
+    });
+}
+
+function purchasePrompt(){
+    inquirer.prompt([
+    {
+        name: "ID",
+        type: "input",
+        message: "Enter ID of Item you would like to buy.",
+        filter: Number
+    },
+    { 
+        name:"Quantity",
+		type:"input",
+		message:"Enter quantity of item you would like to buy.",
+		filter: Number
+    },
+    ]).then(function(answers){
+        const quantityNeeded = answers.Quantity;
+        const IDrequested = answers.ID;
+        purchaseOrder(IDrequested, quantityNeeded)
+    });
+};
+
+function purchaseOrder(ID, amtNeeded){
+    connection.query('SELECT * FROM products WHERE item_id = ' + ID, function(err,res){
+        if(err){console.log(err)};
+        if(amtNeeded <= res[0].stock_quantity){
+            const totalCost = res[0].price * amtNeeded;
+            console.log("Order is in stock.");
+            console.log("Total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost);
+
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
+        } else{
+            console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");     
+        };
+        displayProducts();
+    });
+};
+
+displayProducts();
