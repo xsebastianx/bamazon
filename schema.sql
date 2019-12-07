802901abae5ffdf8f9cc0ef386be385a10764b3d
Sebastian Arnavat <sebastianarnavat@Sebastians-MacBook-Pro.local>
2019-12-07 11:30:10 -0500

    Updated

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
 product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR(100) NOT NULL,
 price DECIMAL (10,2) NOT NULL,
-stock_quantity INTEGER NOT NULL,
+stock_quantity INTEGER (20) NOT NULL,
 PRIMARY KEY (item_id)
 );
 
