8827fa52d23f83d6e5a70b5456718eaa6cba1a9b
Sebastian Arnavat <sebastianarnavat@Sebastians-MacBook-Pro.local>
2019-12-11 19:00:00 -0500

    Able to select product and amount

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,19 +5,20 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user:'root',
-    password: '',
+    password: 'password',
     database: 'bamazon'
 
 });
 
-function start (){
+function displayProducts(){
     connection.query('SELECT * FROM products', function(err, res){
         if(err) throw err;
-        console.log('Welcome to Bamazon');
-        console.log('____________________________________________');
+        console.log("Welcome to Bamazon, can I take your order?")
+        console.log("-------------------------------------------------------------------------------")
+
         for(var i = 0; i<res.length;i++){
-            console.log("ID: " + res[i].item_id + " | " +  "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Quantity " + res[i].stock_quantity);
-            console.log('____________________________________________');
+            console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Quantity: " + res[i].stock_quantity);
+            console.log("------------------------------------------------------------------------------------------") 
         }
 
         console.log(' ');
@@ -25,21 +26,48 @@ function start (){
             {
                 type: "input",
                 name: "id",
-                message: "Please select and item ID that you would like to purchase.",
+                message: "Please enter the ID of the product you would like to purchase.",
                 validate: function(value){
                     if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
                         return true;
-                    } else{
+                      } else{
                         return false;
-                    }
+                      }
                 }
             },
             {
                 type: "input",
                 name: "qty",
-                message: "Please indicate the amount you would like to purchase.",
-                
+                message: "Please enter the amount you would like to purchase.",
+                validate: function(value){
+                    if(isNaN(value)){
+                    return false;
+                    } else{
+                    return true;
+                    }
+                }
             }
-        ])
-    })
-}
+        ]).then(function(answers){
+            const quantityNeeded = answers.qty;
+            const idRequested = answers.id;
+            purchaseOrder(idRequested, quantityNeeded);
+        });
+    });
+
+    function purchaseOrder(id, amtNeeded){
+        connection.query('SELECT * FROM products WHERE item_id = ' + id, function(err, res){
+            if(err){console.log(err)};
+            if(amtNeeded <= res[0].stock_quantity){
+                const totalCost = res[0].price * amtNeeded;
+                console.log("Your order is in stock!");
+                console.log("Your total cost for " + amtNeeded + " " + res[0].product_name + "is " + totalCost);
+                connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + id);
+            } else{
+                console.log("Quantity not in stock");
+            };
+            displayProducts();
+        });
+    };
+};
+
+displayProducts();
